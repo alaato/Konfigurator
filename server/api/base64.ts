b3bb306919b8a60749352c31f883608d1a952248
Alaa Atarban <toalaa9@gmail.com>
2024-08-02 15:34:05 +0200

    error

--- server/api/base64.ts
+++ server/api/base64.ts
@@ -1,8 +1,12 @@
 import base64Img from 'base64-img';
 
-export default defineEventHandler(async (event) => {
-
-
-  const base64 = base64Img.base64Sync('public/TCS_Logo_RGB.jpg')
+export default defineEventHandler((event) => {
+  const base64 = base64Img.base64Sync('@/public/TCS_Logo_RGB.jpg')
+  if(!base64) {
+    throw createError({
+      statusCode: 500,
+      statusMessage: 'Something went wrong'
+    })
+  }
   return base64
 })
